e53d9618c376ebbc2f6a522aa7529213584fa5d8
Martin Kuczynski <129275100+markuczy@users.noreply.github.com>
2026-06-23 09:48:48 +0200

    fix: copy all json files

--- scripts/copy-dist-assets.js
+++ scripts/copy-dist-assets.js
@@ -23,13 +23,34 @@ function copyDirectory(sourceDir, targetDir) {
   }
 }
 
+function copyJsonFilesRecursively(sourceDir, targetDir) {
+  if (!fs.existsSync(sourceDir)) {
+    return
+  }
+
+  fs.mkdirSync(targetDir, { recursive: true })
+
+  for (const item of fs.readdirSync(sourceDir, { withFileTypes: true })) {
+    const sourcePath = path.join(sourceDir, item.name)
+    const targetPath = path.join(targetDir, item.name)
+
+    if (item.isDirectory()) {
+      copyJsonFilesRecursively(sourcePath, targetPath)
+    } else if (item.isFile() && item.name.endsWith('.json')) {
+      fs.copyFileSync(sourcePath, targetPath)
+    }
+  }
+}
+
 const importsDir = path.join(root, 'imports')
 const importsScriptsDir = path.join(root, 'imports-scripts')
+const srcDir = path.join(root, 'src')
 const distImportsDir = path.join(distDir, 'imports')
 const distImportsScriptsDir = path.join(distDir, 'imports-scripts')
 
 copyDirectory(importsDir, distImportsDir)
 copyDirectory(importsScriptsDir, distImportsScriptsDir)
+copyJsonFilesRecursively(srcDir, distDir)
 
 const rootPackageJsonPath = path.join(root, 'package.json')
 const rootPackageJson = JSON.parse(fs.readFileSync(rootPackageJsonPath, 'utf8'))
