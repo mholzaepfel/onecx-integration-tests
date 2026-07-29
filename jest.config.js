907a7874c4fd0a7e654e16e9801b53403b97f9c8
mholzaepfel <204693613+mholzaepfel@users.noreply.github.com>
2026-07-29 13:08:51 +0200

    fix: remove nx from integration-tests

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  displayName: 'integration-tests',
+  snapshotFormat: { escapeString: true, printBasicPrototype: true },
+  testEnvironment: 'node',
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov', 'text', 'text-summary', 'html'],
+  reporters: [
+    'default',
+    [
+      'jest-sonar',
+      {
+        outputDirectory: 'reports',
+        outputName: 'sonarqube_report.xml',
+        reportedFilePath: 'absolute',
+      },
+    ],
+  ],
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  testMatch: ['<rootDir>/src/lib/**/*.spec.ts', '<rootDir>/src/it-runner/**/*.spec.ts'],
+  coverageDirectory: '<rootDir>/reports/coverage',
+}
